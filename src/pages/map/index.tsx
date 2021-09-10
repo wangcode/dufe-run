b6ef62f0cb965c2433286b49a1d2590d587ee9cb
Jiawang Liu <ext.liujiawang1@jd.com>
2021-09-10 13:42:02 +0800

    add

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, ImageOverlay, Polyline, FeatureGroup, Circle } from 'react-leaflet'
+import React, { useEffect, useMemo, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, ImageOverlay, Polyline, FeatureGroup, Circle, useMap } from 'react-leaflet'
 
 import MapPNG from './img/map.png';
 
-import { CRS, LatLngExpression } from 'leaflet';
+import { CRS, latLng, LatLngExpression } from 'leaflet';
+
+import GeoUtils from 'leaflet-geometryutil';
+
 // import { EditControl } from "react-leaflet-draw"
 
 // import "react-leaflet-draw/dist/leaflet.draw.css";
@@ -15,6 +18,7 @@ import PolylineJSON from './polyline.json';
 import UserToolBar from './components/userToolBar';
 import RunToolBar from './components/runToolBar';
 import UserMark from './components/userMark';
+import RunLine from './components/runLine';
 
 const Points: {position: LatLngExpression}[] = [
     {
@@ -22,18 +26,65 @@ const Points: {position: LatLngExpression}[] = [
     }
 ]
 
+const Interval = 20000000;
+
 function Map() {
+
+  const [myStep, setMyStep] = useState(0)
+
+  useEffect(() => {
+    if(myStep===100000) setMyStep(0)
+    setTimeout(() => {
+      setMyStep(myStep+100)
+    }, 100)
+    // setInterval(() => {
+    //   // console.log(myStep+10)
+    //   // setMyStep(myStep+10)
+    // }, 10)
+  }, [myStep])
+
+  // const Path = useMemo(() => {
+  //   return PolylineJSON.map(point => latLng(point[0], point[1]))
+  // }, [PolylineJSON])
+
+  // const {lengths, totalLength} = useMemo(() => {
+  //   return {
+  //     lengths: GeoUtils.accumulatedLengths(Path),
+  //     totalLength: GeoUtils.accumulatedLengths(Path).reduce((a, b) => a + b, 0)
+  //   }
+  // }, [Path])
+
+  // const totalPoints = Math.floor(totalLength / Interval);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     const ratios = [];
+  //     for (let i = 0; i <= totalPoints; i++) {
+  //       const ratio = i / totalPoints;
+  //       ratios.push(ratio);
+  //     }
+  //     console.log(ratios)
+  //   }, 0);
+  // }, [])
+
+
+  // const points = ratios.map((ratio) =>
+  //   GeoUtils.interpolateOnLine(map, Path, ratio)
+  // );
+
+
+  // console.log(points)
 
   return (
     <div className={styles.main}>
-      <div className={styles.userToolBar}>
+      {/* <div className={styles.userToolBar}>
         <UserToolBar self={true} />
         <UserToolBar self={false} />
       </div>
       <div className={styles.runToolBar}>
         <RunToolBar />
         <UserMark />
-      </div>
+      </div> */}
       <MapContainer
         crs={CRS.Simple}
         style={{width: "100vw", height:"100vh"}}
@@ -49,23 +100,13 @@ function Map() {
         maxBoundsViscosity={1}
       >
         <ImageOverlay url={MapPNG} bounds={[[0,0], [3361,3839]]} />
+        {/* <Polyline positions={Path} /> */}
+        <RunLine myStep={myStep} />
         {/* <FeatureGroup>
-          <EditControl
-            position='topright'
-            onEdited={(a: any) => console.log("Edited", a)}
-            onCreated={(a: any) => console.log("Created", a)}
-            onDeleted={(a: any) => console.log("Deleted", a)}
-            draw={{
-              rectangle: false
-            }}
-          />
-          <Polyline positions={PolylineJSON as any} />
-        </FeatureGroup> */}
-        <FeatureGroup>
           {Points.map(point => {
               return <Marker key={point.position.toString()} position={point.position} />
           })}
-        </FeatureGroup>
+        </FeatureGroup> */}
       </MapContainer>
     </div>
   )
