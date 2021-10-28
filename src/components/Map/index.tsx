0e2fa1849c7c5ef207f61b2c068d1329e8e0c827
Wang <wang@liujia.wang>
2021-10-29 00:07:41 +0800

    更换新地图

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,7 +3,7 @@ import { CRS } from 'leaflet';
 import { MapContainer, ImageOverlay } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 
-import MapPNG from './map.png';
+import MapPNG from './map45.jpg';
 
 interface MapProps {
   style?: CSSProperties;
@@ -27,13 +27,13 @@ const Map: React.FC<MapProps> = ({
       zoomControl={false}
       zoomDelta={0.5}
       zoomSnap={0.5}
-      maxBounds={[[0, 0], [3361, 3839]]}
+      maxBounds={[[0, 0], [3361, 3845]]}
       // 超出拖动弹性值
       maxBoundsViscosity={1}
     >
       {/* <MapRoute myStep={mySteps?.data?.allStep || userDetail?.data?.allStep || "0"} /> */}
       {props.children}
-      <ImageOverlay url={MapPNG} bounds={[[0, 0], [3361, 3839]]} />
+      <ImageOverlay url={MapPNG} bounds={[[0, 0], [3361, 3845]]} />
     </MapContainer>
   )
 }
