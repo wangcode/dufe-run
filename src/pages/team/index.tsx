f5eabb0be17bd06a972ffddfe19b296e99573b74
Wang <wang@liujia.wang>
2021-11-02 21:12:25 +0800

    修复图片显示问题 修复排行榜被遮挡问题

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -65,14 +65,16 @@ const Team = () => {
           <TeamRankItem single rank={myTeam.data.allRank} name={myTeam.data.name} people={myTeam.data.personNum} length={`${myTeam.data.aveKm || "0"}KM`} />
         </div>}
 
-        <Card title="战队排行榜">
-          {teams.data?.map((item, index) => (
-            <div key={item.id} className={styles.teamListItem} onClick={() => setTeamId(item.id.toString())}>
-              <TeamRankItem rank={index + 1} name={item.name} people={item.personNum || 0} length={`${item.aveKm || 0}KM`} />
-            </div>
-          ))}
-          {teams.data?.length === 0 && <Empty description="暂无排行" />}
-        </Card>
+        <div className={styles.card}>
+          <Card title="战队排行榜">
+            {teams.data?.map((item, index) => (
+              <div key={item.id} className={styles.teamListItem} onClick={() => setTeamId(item.id.toString())}>
+                <TeamRankItem rank={index + 1} name={item.name} people={item.personNum || 0} length={`${item.aveKm || 0}KM`} />
+              </div>
+            ))}
+            {teams.data?.length === 0 && <Empty description="暂无排行" />}
+          </Card>
+        </div>
       </div>
 
       {!!userId && <Map style={{ height: "70vh", marginBottom: "30vh" }}>
