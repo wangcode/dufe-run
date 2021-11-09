e4ad17ec825e6ef5cb32d63a0e783114225b6e7b
Wang <wang@liujia.wang>
2021-11-09 23:24:12 +0800

    修复头像问题

--- src/components/Base/Avatar/index.tsx
+++ src/components/Base/Avatar/index.tsx
@@ -4,40 +4,40 @@ import { Avatar as ANTDAvatar } from 'antd';
 
 import styles from './index.module.scss';
 
-const AvatarSize: {small: React.CSSProperties, middle: React.CSSProperties, large: React.CSSProperties} = {
-    small: {
-        width: 35,
-        height: 35,
-        fontSize: "24px",
-        lineHeight: "35px",
-        textAlign: "center"
-    },
-    middle: {
-        width: 40,
-        height: 40,
-        fontSize: "24px",
-        lineHeight: "40px",
-        textAlign: "center"
-    },
-    large: {
-        width: 58,
-        height: 58,
-        fontSize: "26px",
-        lineHeight: "58px",
-        textAlign: "center"
-    }
+const AvatarSize: { small: React.CSSProperties, middle: React.CSSProperties, large: React.CSSProperties } = {
+  small: {
+    width: 35,
+    height: 35,
+    fontSize: "24px",
+    lineHeight: "35px",
+    textAlign: "center"
+  },
+  middle: {
+    width: 40,
+    height: 40,
+    fontSize: "24px",
+    lineHeight: "40px",
+    textAlign: "center"
+  },
+  large: {
+    width: 58,
+    height: 58,
+    fontSize: "26px",
+    lineHeight: "58px",
+    textAlign: "center"
+  }
 }
 
 interface AvatarProps {
-    src?: string;
-    radius?: number;
-    size?: keyof typeof AvatarSize;
-    text?: string;
+  src?: string;
+  radius?: number;
+  size?: keyof typeof AvatarSize;
+  text?: string;
 }
 
-const Avatar: React.FC<AvatarProps> = ({ src, size="middle", radius=10, text }) => {
+const Avatar: React.FC<AvatarProps> = ({ src, size = "middle", radius = 10, text }) => {
 
-    return <ANTDAvatar className={styles.avatar} src={src} style={{borderRadius: radius, ...AvatarSize[size]}} >{text?.split("")[0]}</ANTDAvatar>
+  return <ANTDAvatar className={styles.avatar} src={src} style={{ borderRadius: radius, ...AvatarSize[size] }} >{text?.[0]}</ANTDAvatar>
 
 }
 
