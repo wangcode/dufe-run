9486bb8b076051b8a671c75206ff00b941ceae33
Wang <wang@liujia.wang>
2021-11-09 23:21:11 +0800

    fix: 修复业务域名问题

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
 import { Spin } from 'antd';
 import { useSearchParam } from 'react-use';
+import { useHistory } from 'react-router';
 
 
 const Login = () => {
+
+  const history = useHistory()
 
   const token = useSearchParam("token")
   const refreshToken = useSearchParam("refreshToken")
@@ -12,7 +15,7 @@ const Login = () => {
     if (token && refreshToken) {
       localStorage.setItem("token", token)
       localStorage.setItem("refreshToken", refreshToken)
-      window.location.replace("/webview")
+      history.replace('/')
     }
   }, [token, refreshToken])
 
